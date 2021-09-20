db9be69fb526091831bf586fdc4f5df29f8fadc8
JBD <lio222@hanmail.net>
2021-09-20 18:30:38 +0900

    2021-09-20 Python Convert function

---
+++ PythonStudy/PythonStudy02/PythonStudy02.py
@@ -0,0 +1,27 @@
+"""
+PROJECT NAME        : Python study
+PROJECT INFORMATION :
+CREATE DATE         : 2021-09-20
+WRITER              :
+MODIFIER            :
+MODIFY DATE         :
+"""
+
+#Convert funttion
+# convert float to int 
+vF = 3.14 
+print(vF)
+print(int(vF))
+
+#convert int to float 
+vI = 3
+print(vI)
+print(float(vI))
+
+#convert int to string 
+print(str(vI))
+
+#convert str to list 
+vS = '3.14'
+print(list(vS))
+ 
