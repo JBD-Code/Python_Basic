6f9b55269b749006c516072927b4e037f232aa3d
JBD <lio222@hanmail.net>
2021-09-20 18:10:12 +0900

    초기 커밋.

---
+++ PythonStudy/PythonStudy/PythonStudy.py
@@ -0,0 +1,76 @@
+
+"""
+PROJECT NAME        : Python study
+PROJECT INFORMATION :
+CREATE DATE         : 2021-09-20
+WRITER              :
+MODIFIER            :
+MODIFY DATE         :
+"""
+#print function
+print("HELLO WORLD")
+print([1, 2 ,3])
+
+#input function
+#name = input("What is your name?")
+
+#variable
+#Numeric
+my_int1 = 1
+my_int2 = -2
+my_int3 = 4096
+my_float1 = 1.0
+my_float2 = 2.0
+my_float3 = 3.0
+print(my_int1)
+print(my_int2)
+print(my_int3)
+print(my_float1)
+print(my_float2)
+print(my_float3)
+
+#String
+my_str1 = 'a'
+my_str2 = 'b'
+my_str3 = 'c'
+print(my_str1)
+print(my_str2)
+print(my_str3)
+
+#Boolean
+my_bool1 = True
+my_bool2 = False
+my_bool3 = 1 < 3
+my_bool4 = 5==4
+print(my_bool1)
+print(my_bool2)
+print(my_bool3)
+print(my_bool4)
+
+#List 
+my_list =[]
+my_list.append(123)
+my_list.append('list')
+my_list.append(3.14)
+print(my_list)
+
+#Tuple 
+my_tuple1 = () 
+my_tuple2 = (2, )
+my_tuple3 = ('a', 'b', 'c')
+my_tuple4 = 3.14, 'Python', True 
+
+for value in my_tuple4 : 
+	print(value)
+print()
+
+#Dictionary 
+my_dict = {} 
+my_dict[1] = 'a'
+my_dict['b'] = 2
+my_dict['c'] = 3
+my_dict[4] = 'e'
+
+for key in my_dict:
+	print("key :" + str(key))
+print()
